d4b4d611c1cfba6d93523e676ed772546d1a5801
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-24 19:48:04 -0400

    Adds 3 products to the seeds folder to use during testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create({ name: "Lord of the Rings", price: 20, image_url: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg", description: "Fantasy fiction" })
+
+Product.create({ name: "The Conjuring", price: 25, image_url: "https://m.media-amazon.com/images/M/MV5BMTM3NjA1NDMyMV5BMl5BanBnXkFtZTcwMDQzNDMzOQ@@._V1_.jpg", description: "Horror" })
+
+Product.create({ name: "Inside Out", price: 15, image_url: "https://imageio.forbes.com/blogs-images/jvchamary/files/2015/08/Inside-Out-First-Look-RGB-d150_20mpub.pub16.101-1940x1091.jpg?format=jpg&height=900&width=1600&fit=bounds", description: "Kids" })
