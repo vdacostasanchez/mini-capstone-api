21754d69c16550445b818abd9c9c06ab83909ab3
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-10-24 20:02:07 -0400

    Adds a route to get a random movie from the files in database

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   def all_products
-    contacts = Product.all
-    render json: contacts.as_json
+    movies = Product.all
+    render json: movies.as_json
+  end
+
+  def random_movie
+    index = rand(1..3)
+    movie = Product.find(index)
+    render json: movie.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/all-products", controller: "products", action: "all_products"
+
+  get "/random-movie", controller: "products", action: "random_movie"
 end
